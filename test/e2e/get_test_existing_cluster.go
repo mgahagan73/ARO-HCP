4f5be993fb523970ae009e00cbe48b8faae59e9b
Mike Gahagan <mgahagan@redhat.com>
2025-04-02 16:23:04 -0400

    rename existing cluster test, add test which uses kubeconfig file

--- test/e2e/get_test_existing_cluster.go
+++ test/e2e/get_test_existing_cluster.go
@@ -25,22 +25,38 @@ var _ = Describe("Cluster login operation", func() {
 
 	clusterName := os.Getenv("CLUSTER_NAME")
 	kubeadminPassword := os.Getenv("KUBEADMIN_PASSWORD")
-	//kubeconfig := os.Getenv("KUBECONFIG")
+	clusterKubeconfig := os.Getenv("CLUSTER_KUBECONFIG")
+	clusterApiUrl := ""
 
-	It("Log in to existing cluster with kubeadmin password", labels.Medium, labels.Positive, func(ctx context.Context) {
-		By("Get cluster api URL from RP")
+	It("Confirm cluster has been created successfully", labels.Medium, labels.Positive, func(ctx context.Context) {
+		By("Checking Provisioning state with RP")
 		out, err := clustersClient.Get(ctx, customerRGName, clusterName, nil)
 		Expect(err).To(BeNil())
 		Expect(string(*out.Properties.ProvisioningState)).To(Equal(("Succeeded")))
-		clusterApiUrl := string(*out.Properties.API.URL)
+	})
+	It("Log in to existing cluster with kubeadmin password", labels.Medium, labels.Positive, func(ctx context.Context) {
+		By("Get cluster api URL from RP")
+		Expect(kubeadminPassword).ToNot(BeNil())
+		out, err := clustersClient.Get(ctx, customerRGName, clusterName, nil)
+		Expect(err).To(BeNil())
+		clusterApiUrl = string(*out.Properties.API.URL)
 		Expect(clusterApiUrl).To(ContainSubstring("https://"))
 		By("Log in with oc command")
 		//oc_command := fmt.Sprintf("oc login -u kubeadmin -p " + kubeadminPassword + " " + clusterApiUrl)
 		oc_command := "oc --insecure-skip-tls-verify login -u kubeadmin -p " + kubeadminPassword + " " + clusterApiUrl
 		stdout, stderr, err := cmdline.RunCMD(oc_command)
+		Expect(err).To(BeNil())
 		Expect(stderr).ToNot(ContainSubstring("error"))
 		Expect(stdout).To(ContainSubstring("Login successful."))
+	})
+	It("Can access cluster with oc command using kubeconfig file", labels.Medium, labels.Positive, func(ctx context.Context) {
+		Expect(clusterKubeconfig).ToNot(BeNil())
+		By("Get projects on cluster")
+		oc_command := "KUBECONFIG=" + clusterKubeconfig + " oc get projects"
+		stdout, stderr, err := cmdline.RunCMD(oc_command)
 		Expect(err).To(BeNil())
+		Expect(stderr).ToNot(ContainSubstring("error"))
+		Expect(stdout).To(ContainSubstring("default"))
 	})
 
 })
