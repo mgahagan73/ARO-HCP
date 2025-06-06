34c76bcca20a3f94f07b68924e47358b03825667
Mike Gahagan <mgahagan@redhat.com>
2025-06-06 16:19:55 -0400

    Update cluster test to use integration rather than env. vars.

--- test/e2e/get_test_existing_cluster.go
+++ test/e2e/get_test_existing_cluster.go
@@ -2,39 +2,52 @@ package e2e
 
 import (
 	"context"
-	"os"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	api "github.com/Azure/ARO-HCP/internal/api/v20240610preview/generated"
 	"github.com/Azure/ARO-HCP/test/util/cmdline"
+	"github.com/Azure/ARO-HCP/test/util/integration"
 	"github.com/Azure/ARO-HCP/test/util/labels"
 )
 
-var _ = Describe("Cluster login operation", func() {
+var _ = Describe("Confirm HCPCluster is operational", func() {
 
 	var (
-		clustersClient *api.HcpOpenShiftClustersClient
+		clustersClient    *api.HcpOpenShiftClustersClient
+		clusterKubeconfig string
+		customerEnv       *integration.CustomerEnv
+		clusterInfo       *integration.Cluster
 	)
 
 	BeforeEach(func() {
 		By("Prepare cluster client")
 		clustersClient = clients.NewHcpOpenShiftClustersClient()
+		By("Preparing customer environment values")
+		customerEnv = &e2eSetup.CustomerEnv
 	})
-
-	clusterName := os.Getenv("CLUSTER_NAME")
-	clusterKubeconfig := os.Getenv("CLUSTER_KUBECONFIG")
 
 	It("Confirm cluster has been created successfully", labels.Medium, func(ctx context.Context) {
 		By("Checking Provisioning state with RP")
-		out, err := clustersClient.Get(ctx, customerRGName, clusterName, nil)
+		out, err := clustersClient.Get(ctx, customerEnv.CustomerRGName, clusterInfo.Name, nil)
 		Expect(err).To(BeNil())
 		Expect(string(*out.Properties.ProvisioningState)).To(Equal(("Succeeded")))
 	})
-	It("Can access cluster with oc command using kubeconfig file", labels.Medium, func(ctx context.Context) {
-		Expect(clusterKubeconfig).ToNot(BeNil())
-		By("Get projects on cluster")
+	It("Check access to cluster with oc command using kubeconfig file", labels.Medium, func(ctx context.Context) {
+		By("Getting Kubeconfig from RP")
+		adminPoller, err := clustersClient.BeginRequestAdminCredential(ctx, customerEnv.CustomerRGName, clusterInfo.Name, nil)
+		Expect(err).To(BeNil())
+		for !adminPoller.Done() {
+			status, err := adminPoller.PollUntilDone(ctx, nil)
+			Expect(err).To(BeNil())
+			Expect(status).ToNot(BeNil())
+		}
+		adminCredentialResponse, err := adminPoller.Result(ctx)
+		Expect(err).To(BeNil())
+		Expect(adminCredentialResponse).ToNot(BeNil())
+		clusterKubeconfig = *adminCredentialResponse.Kubeconfig
+		By("Attempting to list projects on cluster and confirm default project is present")
 		oc_command := "KUBECONFIG=" + clusterKubeconfig + " oc get projects"
 		stdout, stderr, err := cmdline.RunCMD(oc_command)
 		Expect(err).To(BeNil())
--- test/e2e/get_test_existing_nodepool.go
+++ test/e2e/get_test_existing_nodepool.go
@@ -8,32 +8,40 @@ import (
 	. "github.com/onsi/gomega"
 
 	api "github.com/Azure/ARO-HCP/internal/api/v20240610preview/generated"
+	"github.com/Azure/ARO-HCP/test/util/integration"
 	"github.com/Azure/ARO-HCP/test/util/labels"
 )
 
 var _ = Describe("Nodepool operation", func() {
 	var (
 		NodePoolsClient *api.NodePoolsClient
+		//nodePoolName    = os.Getenv("NP_NAME")
+		clusterName     = os.Getenv("CLUSTER_NAME")
+		nodePoolOptions *api.NodePoolsClientGetOptions
+		customerEnv     *integration.CustomerEnv
+		nodePools       *[]integration.Nodepool
 	)
 
 	BeforeEach(func() {
 		By("Prepare HCP nodepools client")
 		NodePoolsClient = clients.NewNodePoolsClient()
+		By("Preparing customer environment values")
+		customerEnv = &e2eSetup.CustomerEnv
+		nodePools = &e2eSetup.Nodepools
 	})
 
-	var (
-		nodePoolName    = os.Getenv("NP_NAME")
-		clusterName     = os.Getenv("CLUSTER_NAME")
-		nodePoolOptions *api.NodePoolsClientListByParentOptions
-	)
-
-	It("Get nodepool from cluster", labels.Medium, func(ctx context.Context) {
-		By("Send get request for nodepool")
-		nodePool, err := NodePoolsClient.Get(ctx, customerRGName, clusterName, nodePoolName, (*api.NodePoolsClientGetOptions)(nodePoolOptions))
-		Expect(err).To(BeNil())
-		Expect(nodePool).ToNot(BeNil())
-		By("Check to see nodepool exists and is successfully provisioned")
-		Expect(string(*nodePool.Name)).To(Equal(nodePoolName))
-		Expect(string(*nodePool.Properties.ProvisioningState)).To(Equal("Succeeded"))
+	It("Get each nodepool from cluster", labels.Medium, func(ctx context.Context) {
+		if nodePools != nil {
+			nps := *nodePools
+			for np := range nps {
+				By("Send get request for nodepool")
+				clusterNodePool, err := NodePoolsClient.Get(ctx, customerEnv.CustomerRGName, clusterName, nps[np].Name, nodePoolOptions)
+				Expect(err).To(BeNil())
+				Expect(clusterNodePool).ToNot(BeNil())
+				By("Check to see nodepool exists and is successfully provisioned")
+				Expect(string(*clusterNodePool.Name)).To(Equal(nps[np].Name))
+				Expect(string(*clusterNodePool.Properties.ProvisioningState)).To(Equal("Succeeded"))
+			}
+		}
 	})
 })
